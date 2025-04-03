c740796b40b956006872b77e19eb66bf8bcded89
Tovi Levis <tovi@fhenix.io>
2025-04-03 14:03:27 +0300

    Move functionality from hardhat starter (wallet funding)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import chalk from "chalk";
-import { HDNodeWallet, Wallet } from "ethers";
 import { extendConfig, task, types } from "hardhat/config";
-import { HttpNetworkConfig, HttpNetworkHDAccountsConfig } from "hardhat/types";
 
-import { getFunds } from "./common";
+import { sendFunds } from "./common";
 import { TASK_COFHE_USE_FAUCET } from "./const";
 import { deployMocks } from "./deploy-mocks";
 import { TASK_NODE, TASK_TEST } from "hardhat/builtin-tasks/task-names";
@@ -26,85 +24,45 @@ extendConfig((config, userConfig) => {
       "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
       "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
       "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6"
     ],
   };
 });
 
 type UseFaucetArgs = {
   address?: string;
-  account?: number;
-  url?: string;
+  // account?: number;
+  // url?: string;
 };
 
-// Task to fund an account from the faucet
-task(TASK_COFHE_USE_FAUCET, "Fund an account from the faucet")
+task(TASK_COFHE_USE_FAUCET, "Fund an account from the funder")
   .addOptionalParam("address", "Address to fund", undefined, types.string)
-  .addOptionalParam("account", "account number to fund", 0, types.int)
-  .addOptionalParam(
-    "url",
-    "Optional Faucet URL",
-    // TODO: Check with team to see what the updated faucet url for localcofhe host chain is
-    // - architect 2025-03-27
-    "http://localhost:3000",
-    types.string,
-  )
-  .setAction(async ({ address, account, url }: UseFaucetArgs, { network }) => {
-    const { name: networkName, config: networkConfig } = network;
+  .setAction(async ({ address }: UseFaucetArgs, hre) => {
+    const { network } = hre;
+    const { name: networkName } = network;
 
-    if (networkName !== "localcofhe" && !url) {
+    if (networkName !== "localcofhe") {
       console.info(
         chalk.yellow(
-          `Programmatic faucet only supported for localcofhe. Please provide a faucet url, or use the public testnet faucet at https://faucet.fhenix.zone`,
+          `Programmatic faucet only supported for localcofhe`,
         ),
       );
       return;
     }
 
-    let foundAddress = "";
-    if (Object(networkConfig).hasOwnProperty("url")) {
-      const httpNetworkConfig = networkConfig as HttpNetworkConfig;
-      if (httpNetworkConfig.accounts === "remote") {
-        console.log(chalk.yellow(`Remote network detected, cannot use faucet`));
-        return;
-      } else if (
-        Object(httpNetworkConfig.accounts).hasOwnProperty("mnemonic")
-      ) {
-        const networkObject = httpNetworkConfig.accounts as HttpNetworkHDAccountsConfig;
-
-        const mnemonic = networkObject.mnemonic;
-
-        const path = `${networkObject.path || "m/44'/60'/0'/0"}/${
-          account || networkObject.initialIndex || 0
-        }`;
-
-        const wallet = HDNodeWallet.fromPhrase(mnemonic, "", path);
-
-        foundAddress = wallet.address;
-      } else {
-        const accounts = httpNetworkConfig.accounts as string[];
-        const privateKey = accounts[account || 0];
-        const wallet = new Wallet(privateKey);
-        foundAddress = wallet.address;
-      }
-    }
-
-    const myAddress = address || foundAddress;
-
-    if (!myAddress) {
-      console.info(chalk.red(`Failed to get address from hardhat`));
+    if (!address) {
+      console.info(chalk.red(`Failed to get address to fund`));
       return;
     }
 
-    console.info(chalk.green(`Getting funds from faucet for ${myAddress}`));
+    console.info(chalk.green(`Getting funds from faucet for ${address}`));
 
     try {
-      await getFunds(myAddress, url);
-      console.info(chalk.green(`Success!`));
+      await sendFunds(hre, address);
     } catch (e) {
       console.info(
         chalk.red(
-          `failed to get funds from faucet @ ${url} for ${address}: ${e}`,
+          `failed to get funds from localcofhe for ${address}: ${e}`,
         ),
       );
     }
@@ -136,3 +94,4 @@ task(TASK_NODE, "Deploy mock contracts on hardhat").setAction(
 export * from "./mockUtils";
 export * from "./cofhejs";
 export * from "./result";
+export * from "./common";
--- test/fixture-projects/localcofhe/hardhat.config.ts
+++ test/fixture-projects/localcofhe/hardhat.config.ts
@@ -14,7 +14,7 @@ const config: HardhatUserConfig = {
   defaultNetwork: "localcofhe",
   networks: {
     localfhenix: {
-      url: "http://localhost:8545",
+      url: "http://localhost:42069",
       accounts: {
         mnemonic:
           "demand hotel mass rally sphere tiger measure sick spoon evoke fashion comfort",
