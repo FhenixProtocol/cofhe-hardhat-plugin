7743dc3ee7bb168ba57751fa84b6969edd09866d
architect-dev <halftonedev@gmail.com>
2025-04-09 13:13:28 +0200

    Add `withLogs` wrapper that enables logs only for a function. Improve how the logs look to the user.

--- src/mock-logs.ts
+++ src/mock-logs.ts
@@ -12,22 +12,65 @@ const getDeployedMockTaskManager = async (hre: HardhatRuntimeEnvironment) => {
   return taskManager;
 };
 
-export const mock_setLogging = async (
+const getLoggingEnabled = async (hre: HardhatRuntimeEnvironment) => {
+  const taskManager = await getDeployedMockTaskManager(hre);
+  return await taskManager.logOps();
+};
+
+const setLoggingEnabled = async (
   hre: HardhatRuntimeEnvironment,
-  enable: boolean,
-  closureName?: string,
+  enabled: boolean,
 ) => {
   const taskManager = await getDeployedMockTaskManager(hre);
-  await taskManager.setLogOps(enable);
+  await taskManager.setLogOps(enabled);
+};
 
-  const logsParts = [
-    "[MOCK]",
-    "|",
-    "Logs",
-    enable ? chalk.bold("Enabled") : chalk.bold("Disabled"),
-    closureName ? `within ${chalk.bold(closureName)}` : undefined,
-  ];
+// prettier-ignore
+const printLogsEnabledMessage = (closureMessage: string) => {
+  console.log("┌──────────────────┬──────────────────────────────────────────────────");
+  console.log(`│ [COFHE-MOCKS]    │ ${closureMessage}`);
+  console.log("├──────────────────┴──────────────────────────────────────────────────");
+};
 
-  const logsMessage = logsParts.filter(Boolean).join(" ");
-  console.log(chalk.green(logsMessage));
+// prettier-ignore
+const printLogsBlockEnd = () => {
+  console.log("└─────────────────────────────────────────────────────────────────────");
+};
+
+export const mock_setLoggingEnabled = async (
+  hre: HardhatRuntimeEnvironment,
+  enabled: boolean,
+  closureName?: string,
+) => {
+  try {
+    await setLoggingEnabled(hre, enabled);
+
+    if (enabled) {
+      printLogsEnabledMessage(
+        `${closureName ? `"${chalk.bold(closureName)}" logs:` : "Logs:"}`,
+      );
+    } else {
+      printLogsBlockEnd();
+    }
+  } catch (error) {
+    console.log(chalk.red("mock_setLoggingEnabled error"), error);
+  }
+};
+
+export const mock_withLogs = async (
+  hre: HardhatRuntimeEnvironment,
+  closureName: string,
+  closure: () => Promise<void>,
+) => {
+  const initialState = await getLoggingEnabled(hre);
+
+  await setLoggingEnabled(hre, true);
+  printLogsEnabledMessage(`"${chalk.bold(closureName)}" logs:`);
+  await closure();
+  printLogsBlockEnd();
+
+  // If logs were disabled, disable them again
+  if (!initialState) {
+    await setLoggingEnabled(hre, false);
+  }
 };
